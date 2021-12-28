ac961cfb10d70642802f7cba9886b972f9901bc8
Mayusha04 <mayushachowdary95@gmail.com>
2021-12-28 23:06:22 +0530

    class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+ //       >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+
+//export default App;
+
+import React from "react";
+  
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { change: true };
+  }
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.setState({ change: !this.state.change });
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Click Here!
+        </button>
+        {this.state.change ? (
+          <h1>CGCS bootcamp</h1>
+        ) : (
+          <h1>Full stack web development</h1>
+        )}
+      </div>
+    );
+  }
 }
-
+  
 export default App;
